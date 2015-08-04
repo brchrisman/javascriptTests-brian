c5128ced22a8f475af5b6059d414b1c948bf4548
Scott Loughmiller <scott@scalecomputing.com>
2015-08-04 08:36:01 -0700

    initial commit and transform test

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
 # javascriptTests
 Javascript Interview Tests
+
+## Setup
+Clone the git repo then in the javascriptTests directory run
+
+```
+npm install
+```
+
+You'll need node.js installed in your environment.
+
+
+## Test 1: Transform
+run
+
+```
+mocha tests/transform.mocha.js
+```
+
+to see the failing unit tests.  Fill in the function in transform.js.
+This test is about understanding passing functions as arguments.  This is not a
+ trick question, so a very naive implementation will work.
+
---
+++ transform.js
@@ -0,0 +1,21 @@
+'use strict';
+
+/////////////////////////////////////////////////////////////////////////////
+//  TRANSFORM TEST
+//
+//  Copyright Scale Computing 2015
+//  DO NOT DISTRIBUTE OR REPRODUCE ONLINE IN ANY FORM
+/////////////////////////////////////////////////////////////////////////////
+
+/**
+ * Transform an array to a new array with given function
+ * param a the array to be transformed
+ * param f the function that does the transforming
+ *
+ * Fill in the following function
+ * for simplicity you can assume all arrays will not be sparce and will start
+ * at the 0 index
+ */
+module.exports = function arrayTransform(a, f) {
+
+};
